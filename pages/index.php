2c55cde6cf088f0572db5e67edc85c8fb91a828c
João Pedro <67989586+Hirukinawa@users.noreply.github.com>
2024-01-23 22:54:39 -0300

    v1

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/index.css">
+    <title>Teste</title>
+</head>
+<body>
+    <div class="index">
+        <h1>
+        <?php
+        echo "Site em construção";
+        ?>
+        </h1>
+    </div>
 
-$x = 1;
-
-echo $x;
+</body>
+</html>
